b944245cfdd6b45e6edf5ff06058d800921577f6
ChrisRackauckas-Claude <accounts@chrisrackauckas.com>
2026-08-20 08:24:29 -0400

    Add representative PrecompileTools workload
    Co-Authored-By: Chris Rackauckas <accounts@chrisrackauckas.com>

--- src/BinaryHeaps.jl
+++ src/BinaryHeaps.jl
@@ -38,4 +38,19 @@ include("arrays_as_heaps.jl")
 include("binary_heap.jl")
 include("generic.jl")
 
+using PrecompileTools: @compile_workload, @setup_workload
+
+@setup_workload begin
+    @compile_workload begin
+        values = [4, 1, 3, 2, 16, 9, 10, 14, 8, 7]
+        min_heap = BinaryMinHeap(values)
+        push!(min_heap, 0)
+        pop!(min_heap)
+        extract_all!(BinaryMinHeap(values))
+        heapify(values)
+        nlargest(3, values)
+        nsmallest(3, values)
+    end
+end
+
 end # module
